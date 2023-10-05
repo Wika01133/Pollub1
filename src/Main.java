1e9701299c548617e4adb7300892414640194d3b
Jan kowalski <jan.kowalski@pollub.edu.pl>
2023-10-05 18:54:36 +0200

    kkk4

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+8.
             System.out.println("i = " + i);
 
-            System.out.println(i + "XD");
+            System.out.println(i + "XD" + 1);
         }
     }
 }
