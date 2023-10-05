402ed9574c6e4790611081bb91633f24fb223ead
Jan kowalski <jan.kowalski@pollub.edu.pl>
2023-10-05 19:22:32 +0200

    com3

--- customer.java
+++ customer.java
@@ -2,6 +2,7 @@ public class customer {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
     }
 
 }
